a789d0414f3c934a332b4855cce5a55fdc0c1ac1
vTanchev <vaskotanchev@yahoo.com>
2023-01-21 17:07:57 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "77058619-a19d-4c7d-b755-c4f14e2b6e72";
+export const MODAL_CLOSE_SEC = 2.5;
